ab8f023ae39662c7bb2abe264fa5ec6fb2a52572
Riley Mathews <rileymathews80@gmail.com>
2024-01-18 22:01:06 -0600

    fixed backup script

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/zsh
 
-source /home/rileymathews/.zshrc
+source /home/rileymathews/server/vaultwarden/.envrc.secret
 echo "backing up sqlite db"
-echo `which sqlite3`
-sqlite3 data/db.sqlite3 ".backup './data/db-backup.sqlite3'"
+sqlite3 data/db.sqlite3 ".backup '$DIR/data/db-backup.sqlite3'"
 echo "backing up to restic repo"
-restic backup -r s3:https://37a8e358fee81bf1f20e08b6ffe72c1d.r2.cloudflarestorage.com:/vaultwarden-restic-repository .
+restic backup /home/rileymathews/server/vaultwarden
