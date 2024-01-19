f59fc9d92d1ec1dc3c6e58ab9c6567f54de05175
Riley Mathews <rileymathews80@gmail.com>
2024-01-18 22:01:30 -0600

    use dir var for backup command

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ source /home/rileymathews/server/vaultwarden/.envrc.secret
 echo "backing up sqlite db"
 sqlite3 data/db.sqlite3 ".backup '$DIR/data/db-backup.sqlite3'"
 echo "backing up to restic repo"
-restic backup /home/rileymathews/server/vaultwarden
+restic backup $DIR 
