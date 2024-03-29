4b0375dbdb882d8ef400bf193596c3db295d0b5e
Riley Mathews <rileymathews80@gmail.com>
2024-03-28 19:43:30 -0500

    fixed backup script

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/zsh
 
 source /home/rileymathews/server/vaultwarden/.envrc.secret
+cd $DIR
 echo "backing up sqlite db"
 sqlite3 data/db.sqlite3 ".backup '$DIR/data/db-backup.sqlite3'"
 echo "backing up to restic repo"
-restic backup $DIR 
+restic backup ./data 
